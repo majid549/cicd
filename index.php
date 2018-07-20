54db0c3195e1d8cbea53c64bdb6da31da343bd6f
majid <md.majid549@gmail.com>
2018-07-20 15:47:22 +0530

    added content
    added content

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
-Welcome to the sample CICD page.
-..........................................
-ayas saxena
+<html>
+<header><title>Temasek Demo</title></header>
+<body>
+Welcome to the Homepage.
+</body>
+</html>
